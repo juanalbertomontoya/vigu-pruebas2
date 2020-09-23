62b25b01669ce4473cbeda90a7e55ae0bd1348b9
juanalbertomontoya <67620654+juanalbertomontoya@users.noreply.github.com>
2020-09-23 10:33:28 -0500

    pirmera subida de vigu
    primera subida de vigu

---
+++ anima-menu.js
@@ -0,0 +1,9 @@
+//animacion de la barra verde
+
+$("#servicios").hover(function () {
+  $("#barra1").toggleClass("on");
+});
+
+$("#contactanos").hover(function () {
+  $("#barra2").toggleClass("on");
+});
