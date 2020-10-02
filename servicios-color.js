72b6df592f411f2367692d8a922525d3d8df4746
juanalbertomontoya <67620654+juanalbertomontoya@users.noreply.github.com>
2020-10-02 10:43:25 -0500

    loader y esfera

--- servicios-color.js
+++ servicios-color.js
@@ -1,3 +1,6 @@
-$('#lanza').hover(function () {
-  $('#recibe').addClass("on");
+$("#brand-logo").hover(function () {
+
+  alert('hola mi gente');
+  
+  $("#servicios-scroll > div > div > div.servicios > div.servicios-branding > div.branding1 > p:nth-child(1) > span.barra-texto").toggleClass("on");
 });
